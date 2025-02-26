ec7d2e4fd84aed30120cab60bd067e3967f1c54e
draxx3 <linacj162@gmail.com>
2025-02-26 16:41:56 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const photos = [
+  "images/1.jfif", 
+  "images/2.jfif", 
+  "images/3.jfif", 
+  "images/4.jfif", 
+  "images/5.jfif", 
+  "images/6.jfif", 
+  "images/7.jfif", 
+  "images/8.jfif"
+];
+let currentPhotoIndex = 0;
+
+function nextPhoto() {
+    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
+    document.getElementById("photo").src = photos[currentPhotoIndex];
+}
+
+function prevPhoto() {
+    currentPhotoIndex = (currentPhotoIndex - 1 + photos.length) % photos.length;
+    document.getElementById("photo").src = photos[currentPhotoIndex];
+}
+
+function goToNextPage() {
+    window.location.href = 'next.html';
+}
